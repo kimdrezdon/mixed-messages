786fdfada51b35ea5a27c4527a4859bf62fdd994
Kim Drezdon <kdrezdon@gmail.com>
2021-07-04 12:05:33 -0500

    Add object of message components

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // For this project, you will build a message generator program. Every time a user runs a program, they should get a new, randomized output. You’re welcome to take the project in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you know of JavaScript syntax so far to build the program and customize it to your liking.
+// Which Michael Scott are you today?
+
+// Store message components
+const messageComponents = {
+	mood: [
+		'happy',
+		'sad',
+		'outraged',
+		'lonely',
+		'funny',
+		'in love',
+		'inspirational',
+		'mischievous'
+	],
+	buddy: ['Pam', 'Jim', 'Dwight', 'Ryan', 'Jan', 'Holly', 'Kevin', 'Stanley'],
+	alter_ego: [
+		'Prison Mike',
+		'Agent Michael Scarn',
+		'Ping',
+		'Michael Klump',
+		'Michael the Magician',
+		'Date Mike',
+		'Michael Scotch'
+	]
+};
